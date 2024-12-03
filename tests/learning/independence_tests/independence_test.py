6bdf41ef0f02ec1bf15e17f8c6a0ec18f27db41b
Jorge Casajús Setién <jcasajus@titaniumindustrialsecurity.com>
2024-12-03 09:40:34 +0000

    independence_tests done

--- tests/learning/independence_tests/independence_test.py
+++ tests/learning/independence_tests/independence_test.py
@@ -2,57 +2,113 @@ import itertools
 
 import numpy as np
 import pandas as pd
-from pybnesian import KMutualInformation, LinearCorrelation, MutualInformation
+from pybnesian import KMutualInformation, LinearCorrelation, MutualInformation, RCoT
 from scipy.stats import pearsonr
-from sklearn.feature_selection import mutual_info_regression
 
 from data import generate_normal_data, generate_normal_data_independent
+
+# from sklearn.feature_selection import mutual_info_regression
+
 
 SIZE = 10000
 SEED = 0
 data = generate_normal_data(SIZE, SEED)
+data_independent = generate_normal_data_independent(SIZE, SEED)
 
 
+# RFE: Test true and false independence
 def test_linear_correlation():
-    df = generate_normal_data(SIZE, SEED)[["A", "B"]]
+    df = data[["A", "B"]]
+    independent_df = data_independent[["A", "B"]]
 
+    # Pybnesian Linear correlation
+    linear_correlation = LinearCorrelation(df)
+    independent_linear_correlation = LinearCorrelation(independent_df)
+    pvalue = linear_correlation.pvalue("A", "B")
+    independent_pvalue = independent_linear_correlation.pvalue("A", "B")
+
+    # scipy pearsonr correlation
     correlations = {}
     columns = df.columns.tolist()
     for col_a, col_b in itertools.combinations(columns, 2):
         correlations[col_a + "__" + col_b] = pearsonr(
             df.loc[:, col_a], df.loc[:, col_b]
         )
-    result = pd.DataFrame.from_dict(correlations, orient="index")
-    result.columns = ["PCC", "p-value"]
+    result = pd.DataFrame.from_dict(
+        correlations, orient="index", columns=["PCC", "p-value"]
+    )
 
-    linear_correlation_pvalue = LinearCorrelation(df).pvalue("A", "B")
+    # Compare correlation values
     np.testing.assert_allclose(
-        np.array([result.loc["A__B", "PCC"]]),
         np.array([df.corr().loc["A", "B"]]),
+        np.array([result.loc["A__B", "PCC"]]),
         rtol=1e-5,
         atol=1e-8,
     )
+    # Compare p-values
     np.testing.assert_allclose(
-        np.array([linear_correlation_pvalue]),
+        np.array([pvalue]),
         np.array([result.loc["A__B", "p-value"]]),
         rtol=1e-5,
         atol=1e-8,
     )
+    # Check whether the p-values are below the significance level
+    assert pvalue < 0.05
+    assert independent_pvalue > 0.05
 
 
 def test_mutual_info():
+    mutual_info = MutualInformation(data)
+    independent_mutual_info = MutualInformation(data_independent)
+
+    # Check whether the mutual information is higher when the variables are dependent
+    mutual_info_value = mutual_info.mi("A", "B")
+    independent_mutual_info_value = independent_mutual_info.mi("A", "B")
+    assert mutual_info_value > independent_mutual_info_value
+
+    # Check whether the p-values are below the significance level
+    pvalue = mutual_info.pvalue("A", "B")
+    independent_pvalue = independent_mutual_info.pvalue("A", "B")
+    assert pvalue < 0.05
+    assert independent_pvalue > 0.05
+
+
+def test_k_mutual_info():
     n_neighbors = 3
-    mutual_info = MutualInformation(data).mi("A", "B")
-    k_mutual_info = KMutualInformation(data, k=n_neighbors).mi("A", "B")
-    sklearn_mutual_info = mutual_info_regression(
-        data[["A"]], data["B"], n_neighbors=n_neighbors
-    )
-    # print("\n", sklearn_mutual_info[0])
-    # print(mutual_info)
-    # print(k_mutual_info)
+    k_mutual_info = KMutualInformation(data, k=n_neighbors)
+    independent_k_mutual_info = KMutualInformation(data_independent, k=n_neighbors)
+
+    # Check whether the mutual information is higher when the variables are dependent
+    k_mutual_info_value = k_mutual_info.mi("A", "B")
+    independent_k_mutual_info_value = independent_k_mutual_info.mi("A", "B")
+    assert k_mutual_info_value > independent_k_mutual_info_value
+
+    # Check whether the p-values are below the significance level
+    pvalue = k_mutual_info.pvalue("A", "B")
+    independent_pvalue = independent_k_mutual_info.pvalue("A", "B")
+    assert pvalue < 0.05
+    assert independent_pvalue > 0.05
+
+    # RFE: Results vary with scikit-learn, why?
+    # sklearn_k_mutual_info_value = mutual_info_regression(
+    #     data[["A"]], data["B"], n_neighbors=n_neighbors
+    # )[0]
+    # print(k_mutual_info_value)
+    # print("\n", sklearn_k_mutual_info_value)
     # np.testing.assert_allclose(
-    #     sklearn_mutual_info,
-    #     np.array([k_mutual_info]),
+    #     sklearn_k_mutual_info_value,
+    #     np.array([k_mutual_info_value]),
     #     rtol=1e-5,
     #     atol=1e-8,
     # )
+
+
+def test_rcot():
+    rcot = RCoT(data, random_fourier_xy=5, random_fourier_z=100)
+    independent_rcot = RCoT(data_independent, random_fourier_xy=5, random_fourier_z=100)
+
+    p_value = rcot.pvalue("A", "B")
+    independent_p_value = independent_rcot.pvalue("A", "B")
+
+    assert p_value < 0.05
+    assert independent_p_value > 0.05
